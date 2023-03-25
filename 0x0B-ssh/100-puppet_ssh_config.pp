22ddc69dd4fa8b79aea62818ac868f4f3dd18014
markbrlaise <markkiguli10@gmail.com>
2023-03-25 23:36:43 +0000

    fixing code

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,13 @@
 # Setting up my client config file
-include stdlib
 
+include stdlib
 file_line { 'Turn off passwd auth':
-  ensure  => present,
   path    => '/etc/ssh/ssh_config',
   line    => '	  PasswordAuthentification no',
   replace => true,
 }
 
-file_line { 'Delare identity file':
-  ensure  => present,
+file_line { 'Declare identity file': 
   path    => '/etc/ssh/ssh_config',
   line    => '     IdentityFile ~/.ssh/school',
   replace => true,
