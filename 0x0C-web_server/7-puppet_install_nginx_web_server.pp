af7b300cc09f042480014a6441e82059834ebdda
mark blaise <markkiguli10@gmail.com>
2023-03-29 14:04:40 +0100

    the damn typos

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,23 @@
+# puppet manifest to install nginx
+
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file { 'index.html':
+  ensure  => 'present',
+  content => 'Hello World!',
+  path    => '/var/www/html/',
+}
+
+file_line { '# yyyyyy':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  require => Package['nginx'],
+}
